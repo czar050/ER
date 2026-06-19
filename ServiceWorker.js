c0941cb47073f869593dc2b76356fad878c5c394
czar050 <fenix717@mail.ru>
2026-06-19 22:08:32 +0300

    45

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "czar050-Endless Ride-0.1.0";
+const contentToCache = [
+    "Build/45GZip.loader.js",
+    "Build/45GZip.framework.js.unityweb",
+    "Build/45GZip.data.unityweb",
+    "Build/45GZip.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
